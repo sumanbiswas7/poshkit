5a35e469048a8f7c35be027ca4890f8432281f6c
suman <ronaldosuman97@gmail.com>
2023-03-06 22:23:17 +0530

    fix: toggle-dark-mode docs theming color change

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,13 @@
 import { dark, light } from "./theme";
+import { DocsContainer } from "./DocsContainer";
 
 export const parameters = {
   darkMode: {
     light,
     dark,
+    current: light,
+  },
+  docs: {
+    container: DocsContainer,
   },
 };
