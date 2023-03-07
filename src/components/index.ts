c2b6b006863b91610e4cca06a798fa83131733b7
suman <ronaldosuman97@gmail.com>
2023-03-07 20:51:50 +0530

    fix: buttonv5 export for module

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import { ButtonV1 } from "./ButtonV1"
 import { ButtonV2 } from "./ButtonV2"
 import { ButtonV3 } from "./ButtonV3"
 import { ButtonV4 } from "./ButtonV4"
+import { ButtonV5 } from "./ButtonV5"
 
-export { ButtonV1, ButtonV2, ButtonV3, ButtonV4 }
+export { ButtonV1, ButtonV2, ButtonV3, ButtonV4, ButtonV5 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { ButtonV1, ButtonV2, ButtonV3, ButtonV4 } from './components';
-export { ButtonV1, ButtonV2, ButtonV3, ButtonV4 }
+import { ButtonV1, ButtonV2, ButtonV3, ButtonV4, ButtonV5 } from './components';
+export { ButtonV1, ButtonV2, ButtonV3, ButtonV4, ButtonV5 }
 
 // export * from './components';
 
